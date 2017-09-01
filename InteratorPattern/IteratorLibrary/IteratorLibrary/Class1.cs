72257ec7911dcc778ea4e6daf8b9978a1e5e8464
dylanstorts <d-storts@onu.edu>
2017-08-31 21:44:08 -0400

    First attempt to Iterator Pattern
    Code has not been tested and I only have been writing in what I believe I need.

---
+++ InteratorPattern/IteratorLibrary/IteratorLibrary/Class1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace IteratorLibrary
+{
+    //class MainApp
+    //{
+        
+    //    /// Entry point into console application.
+    //    static void Main()
+    //    {
+    //        // Build a collection
+    //        Collection collection = new Collection();
+    //        collection[0] = new Item("Item 0");
+    //        collection[1] = new Item("Item 1");
+    //        collection[2] = new Item("Item 2");
+    //        collection[3] = new Item("Item 3");
+    //        collection[4] = new Item("Item 4");
+    //        collection[5] = new Item("Item 5");
+    //        collection[6] = new Item("Item 6");
+    //        collection[7] = new Item("Item 7");
+    //        collection[8] = new Item("Item 8");
+
+    //        // Create iterator
+    //        Iterator iterator = collection.CreateIterator();
+
+    //        // Skip every other item
+    //        iterator.Step = 2;
+
+    //        Console.WriteLine("Iterating over collection:");
+
+    //        for (Item item = iterator.First();
+    //            !iterator.IsDone; item = iterator.Next())
+    //        {
+    //            Console.WriteLine(item.Name);
+    //        }
+
+    //        // Wait for user
+    //        Console.ReadKey();
+    //    }
+    //}
+
+    /// Item is just the type of object in this aggregate for this iterator
+
+    public class Item
+    {
+        private string name;
+
+        public Item(string new_name)
+        {
+            this.name = new_name;
+        }
+
+        public string Name
+        {
+            get { return name;  }
+        }
+    }
+
+    /// The 'Aggregate' interface
+
+    interface AbstractCollection
+    {
+        Iterator CreateIterator();
+    }
+
+    /// The 'ConcreteAggregate' class
+
+    class Collection : AbstractCollection
+    {
+        private ArrayList _items = new ArrayList();
+
+        public Iterator CreateIterator()
+        {
+            return new Iterator(this);
+        }
+
+        // Gets item count
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+
+        // Indexer
+        public object this[int index]
+        {
+            get { return _items[index]; }
+            set { _items.Add(value); }
+        }
+    }
+
+    /// The 'Iterator' interface
+
+    interface IAbstractIterator
+    {
+        Item First();
+        Item Next();
+        bool IsDone { get; }
+        Item CurrentItem { get; }
+    }
+    
+    /// The 'ConcreteIterator' class
+
+    class Iterator : IAbstractIterator
+    {
+        private Collection _collection;
+        private int _current = 0;
+        private int _step = 1;
+
+        // Constructor
+        public Iterator(Collection collection)
+        {
+            this._collection = collection;
+        }
+
+        // Gets first item
+        public Item First()
+        {
+            _current = 0;
+            return _collection[_current] as Item;
+        }
+
+        // Gets next item
+        public Item Next()
+        {
+            _current += _step;
+            if (!IsDone)
+                return _collection[_current] as Item;
+            else
+                return null;
+        }
+
+        // Gets or sets stepsize
+        public int Step
+        {
+            get { return _step; }
+            set { _step = value; }
+        }
+
+        // Gets current iterator item
+        public Item CurrentItem
+        {
+            get { return _collection[_current] as Item; }
+        }
+
+        // Gets whether iteration is complete
+        public bool IsDone
+        {
+            get { return _current >= _collection.Count; }
+        }
+    }
+}
+
